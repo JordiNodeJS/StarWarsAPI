3b2535edc85a141a82feb8d5ed1ddaac67f97ebb
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-06-29 17:28:58 +0200

    try catch working

--- src/hooks/useFetchImg.jsx
+++ src/hooks/useFetchImg.jsx
@@ -1,10 +1,8 @@
-import { useContext, useState } from 'react'
-import { ContextStarWars } from '../router/ContextStarWarsProvider'
+import { useState } from 'react'
 
 // https://stackoverflow.com/questions/50248329/fetch-image-from-api
 //  https://thewebdev.info/2021/11/14/how-to-fetch-image-from-api-with-react/#:~:text=API%20with%20React%3F-,To%20fetch%20image%20from%20API%20with%20React,can%20use%20the%20fetch%20function.&text=We%20call%20fetch%20with%20the,response%20object%20to%20a%20blob.
 export default function useFetchImg() {
-  const { setError, setLoading } = useContext(ContextStarWars)
 
   const imageUrl =
     'https://starwars-visualguide.com/assets/img/big-placeholder.jpg'
@@ -14,19 +12,19 @@ export default function useFetchImg() {
 
     
       const res = await fetch(image)
+      try {
       if (res.status === 200) {
-        setLoading(true)
-        setError(null)
+
         const imageBlob = await res.blob()
         const imageObjectURL = URL.createObjectURL(imageBlob)
         setImg(imageObjectURL)
-      } else {
+      } else if (res.status === 404) {
         setImg(imageUrl)
-        console.log('error', 'http --> 404 😭💔')
-        setError('💔')
-        setLoading(false)
+        console.log('💩 -->', res.url);
       }
-   
+    } catch (err) {
+      console.log('error en el fetching de datos', err)
+    }
   }
 
   return [img, fetchImage]
